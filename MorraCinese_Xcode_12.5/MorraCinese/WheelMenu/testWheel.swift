89e2b763cf148723054f1eec7bcaaf7a2c41ff62
svarion <alessandro.scisci@edu.itspiemonte.it>
2022-06-08 12:02:14 +0200

    feature Wheel menu
    first try to code wheel menu.
    Still work in progress

---
+++ MorraCinese_Xcode_12.5/MorraCinese/WheelMenu/testWheel.swift
@@ -0,0 +1,35 @@
+//
+//  testWheel.swift
+//  MorraCinese
+//
+//  Created by Alessandro Scisci on 07/06/22.
+//
+
+import SwiftUI
+
+struct testWheel: View {
+    @State var degree = 90.0
+    let array : [moveSlice] =  [moveSlice(val: "✋"),
+                            moveSlice(val: "✂️"),
+                            moveSlice(val: "🗿"),
+                            moveSlice(val: "🦎"),
+                            moveSlice(val: "🖖")
+                            ]
+
+    var body: some View {
+        ZStack (alignment: .center){
+            Color.orange.opacity(0.4).ignoresSafeArea()
+                .hueRotation(Angle(degrees: degree))
+            
+            WheelView(degree: 90.0, array: array, circleSize: 180)
+                .offset(y: 300)
+                .shadow(color: .white, radius: 4, x: 0, y: 0)
+        }
+    }
+}
+
+struct testWheel_Previews: PreviewProvider {
+    static var previews: some View {
+        testWheel()
+    }
+}
