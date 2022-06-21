7d12d5951a38f15090acba3c537c2aa5d84a21fe
Franz <francesco.romeo-work@libero.it>
2022-06-21 10:01:43 +0200

    Added Ai model

---
+++ MorraCinese_Xcode_12.5/MorraCinese/Model/Ai.swift
@@ -0,0 +1,28 @@
+//
+//  Ai.swift
+//  MorraCinese
+//
+//  Created by Francesco Romeo on 21/06/22.
+//
+
+import Foundation
+
+protocol Ai {
+    var availableMoves [Move]
+    var lastMoves: [Move]
+    var usages: [Int]
+    
+    init(availableMoves, lastMoves, usages) {
+        self.availableMoves = availableMoves
+        self.lastMoves = []
+        self.usages = []
+        
+        for move in availableMoves{
+    usages.append(0)
+        }
+    }
+    
+    func move(availableMoves: [Move], lastMoves: [Move]) -> Move {
+    
+    }
+}
