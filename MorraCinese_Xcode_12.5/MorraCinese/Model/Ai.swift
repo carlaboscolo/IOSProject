8abefd22822831778628f9f406a38a1bcdb9da9d
ITS210-AndreaReho <105366916+ITS210-AndreaReho@users.noreply.github.com>
2022-06-28 12:25:29 +0200

    Update Ai.swift
    Ai aggiornata, da testare

--- MorraCinese_Xcode_12.5/MorraCinese/Model/Ai.swift
+++ MorraCinese_Xcode_12.5/MorraCinese/Model/Ai.swift
@@ -24,13 +24,74 @@ class Ai {
         
     }
     
+    
+    
+    
+    //funzione per aumentare la probabilirà dell'utilizzo di una mossa, utilizza dei for incrociati per trovare le mosse contro cui si puo vincere contro le mosse che l'avversario usa più spesso aumentando la probabilità dell'utilizzo delle mosse per vincere in base all'avversario
     func update(playerLastVinningMove: Move){
-        var i: Int
+        
+        /*
+        i:Int contatore iniziale
+        w:Int contatore delle mosse vincenti
+        moveCount:Int contatore della mossa vincente
+        */
+        var i: Int = 0
+        var w:[Int] = 0
+        var moveCount: Int = 0
+        
+        //for per cercare le proprie mosse
         for move in availableMoves{
-            if move == playerLastVinningMove{
-               moveProbabilityBoosts[i]++
+            var j:Int = 0
+            //for per cercare le mosse contro cui si si vince
+            for _ in move.winAgainst {
+                if playerLastVinningMove == move.winAgainst[j]{
+                     // for per cercare la mossa da aumentare la probabilià + aumento di probabilità
+                    for _ in availableMoves{
+                        if move.winAgainst[j] == availableMoves[moveCount] {
+                            moveProbabilityBoosts[moveCount] += 2
+                        }
+                        moveCount++
+                    }
+                }
+                j++
             }
             i++
         }
     }
+    
+    
+    //funzione per calcolare la scelta della Ai, in base hai numeri all'interno del boost la probabilita di ottenere un numero più grande aumenta
+    func choice(lastMoce:[Move]) -> Move{
+        /*
+        I:Int numero incrementale,
+        probability: Int le numeri delle mosse,
+        max: Int numero più grande,
+        maxMove: numero indicativo della mossa da usare
+        */
+        var i: Int = 0
+        var probability: [int]
+        var max:[int] = -99
+        var maxMove:[Move]
+        
+        //for per creare i numeri casuali per la scelta della AI
+        for move in Move {
+             probability[i] = Int.random(in: 0..<10+moveProbabilityBoosts[i])
+        i++
+        }
+        i=0
+        
+        // calcolo del numero massimo per la scelta della mossa
+        for _ in probability {
+            if max < probability[i]{
+                    max = probability[i]
+                    maxMove = availableMoves[i]
+            }
+        i++
+        }
+        
+        //return della mossa
+        return maxMove
+    }
+    
+
 }
